30b1c8ee91f1797797779650ca958d1b92a2cac8
Colby Johnson <colby.johnson@mines.sdsmt.edu>
2016-12-07 14:26:33 -0700

    Started EKF

--- filter.py
+++ filter.py
@@ -4,6 +4,8 @@
 
 import rospy
 import numpy as np
+from scipy import linalg
+from math import cos,sin
 
 from geometry_msgs.msg import Pose2D
 
@@ -21,8 +23,19 @@ def cmdCallback(msg):
     phi_2 = msg.velocities[1]
     
 def filterGPS():
-    global gpsx,gpsy,gpsth,filteredx,filteredy,filteredth,phi_1,phi_2,oldx,oldy,oldth
+    global gpsx,gpsy,gpsth,phi_1,phi_2,xp,sp,xf,P
     
+    V = np.array([[0.001,0,0],[0,0.001,0],[0,0,0.001]])
+    H = np.array([[1,0,0],[0,1,0],[0,0,1]])
+    HT = H.T
+    
+    v = (r/2.0)*(phi_1+phi_2)
+    
+    #x prediction = x (filtered??) + time constant * speed in x direction
+    xp[0]=xf[0,0]+dt*v*np.cos(xp[2])
+    #y prediction = y (filtered??) + time constant * speed in y direction
+    xp[1]=xf[1,0]+dt*v*np.sin(xp[2])
+    #assign our jacobians with the updated data, we will have a 3x3 of jacobian: x,y,0
     
     
     
@@ -52,14 +65,15 @@ filteredx = 0
 filteredy = 0
 filteredth = 0
 
-xp0 = 0
-xp1 = 0
-xf0 = 0
-xf1 = 0
+xp = np.zeros(3)
+sp = np.zeros(3)
+xf = np.zeros((2,3))
+P = np.zeros((2,3,3))
 
 # Define constants
 L = 0.15
 r = 0.05
+dt = 1.0/10.0
 
 # initialize our node
 rospy.init_node('filter')
@@ -71,6 +85,13 @@ rospy.Subscriber("gps", Pose2D, gpsCallback)
 rospy.Subscriber("cmd_joint_traj", JointTrajectory, cmdCallback)
 
 rate = rospy.Rate(10) #10 hz
+
+#publish our initial position of 0
+msg = Pose2D()
+msg.x = 0
+msg.y = 0
+msg.theta = 0
+gpsPub.Publish(msg)
 
 while not rospy.is_shutdown():
     #filter the GPS signal
